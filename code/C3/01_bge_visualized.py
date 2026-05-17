ffaee5782c18abf36109659128a54c34dae0ecd8
encode heart <2570853475@qq.com>
2026-05-17 19:16:02 +0800

    .

--- code/C3/01_bge_visualized.py
+++ code/C3/01_bge_visualized.py
@@ -6,7 +6,7 @@ model = Visualized_BGE(model_name_bge="BAAI/bge-base-en-v1.5",
 model.eval()
 
 with torch.no_grad():
-    text_emb = model.encode(text="datawhale开源组织的logo")
+    text_emb = model.encode(text="hahahahah")
     img_emb_1 = model.encode(image="../../data/C3/imgs/datawhale01.png")
     multi_emb_1 = model.encode(image="../../data/C3/imgs/datawhale01.png", text="datawhale开源组织的logo")
     img_emb_2 = model.encode(image="../../data/C3/imgs/datawhale02.png")
